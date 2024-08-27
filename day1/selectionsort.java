528fa455627332bc2900cd2240025844b4f384f7
Big-Leu <abhishek.rawat21b@iiitg.ac.in>
2024-08-28 00:02:00 +0530

    basic selection sort

---
+++ day1/selectionsort.java
@@ -0,0 +1,36 @@
+package day1;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class selectionsort {
+    public static void swap(int arr[],int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static int[] selection_sort(int arr[]){
+        for(int i =0 ;i <= arr.length-2;i++){
+            int min= i;
+            for(int j = i; j<= arr.length-1;j++){
+              if (arr[min] > arr[j]){
+                    selectionsort.swap(arr,min,j);
+              }
+            }
+        }
+        return arr;
+
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int[] sortedArr = selection_sort(arr);
+        List<Integer> list = Arrays.stream(sortedArr).boxed().collect(Collectors.toList());
+        System.out.println("Sorted array is : " + list);
+        sc.close();
+        sc.close();
+    }
+}
--- day1/test.java
+++ day1/test.java
@@ -1,3 +1,4 @@
+package day1;
 class java{
     public static void main(String[] args) {
         System.out.println("there is always more to code");
